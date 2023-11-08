491b9bc2c3b6226fb1501e3a5ab0cea89ff4b2e9
thienphuc-dev <phucnguyenvuong07@gmail.com>
2023-11-08 21:38:42 +0700

    add community, premium plan and contact to homepg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,15 @@ export default function Home() {
           </div>
           <div className="hr"></div>
           <div className="content">
-
+            <div className="desc">
+              Covid 19 positive or Pneumonitis patient will be treated at
+              home as per doctor order by GNC certified nursing staff.
+            </div>
+            <div className="item"><div className="content">Facilities of blood sugar</div></div>
+            <div className="item"><div className="content">B.P.  Measurement</div></div>
+            <div className="item"><div className="content">Spo2 Measurement</div></div>
+            <div className="item"><div className="content">Ryle's tube insertion</div></div>
+            <div className="item"><div className="content">Dressing facilities are available 24*7</div></div>
           </div>
         </div>
         <div className="mid"></div>
@@ -50,7 +58,83 @@ export default function Home() {
           </div>
           <div className="hr"></div>
           <div className="content">
+            <div className="desc">
+              Covid 19 positive or Pneumonitis patient will be treated at
+              home as per doctor order by GNC certified nursing staff.
+            </div>
+            <div className="item"><div className="content">Facilities of blood sugar</div></div>
+            <div className="item"><div className="content">B.P.  Measurement</div></div>
+            <div className="item"><div className="content">Spo2 Measurement</div></div>
+            <div className="item"><div className="content">Ryle's tube insertion</div></div>
+            <div className="item"><div className="content">Dressing facilities are available 24*7</div></div>
+          </div>
+          <div className="proceed-btn">
+            <div className="content">
+              Contact us
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div className="community">
+        <div className="title">Cộng đồng của chúng tôi</div>
+        <div className="hr"></div>
+        <div className="desc-title">
+          Protect yourself and others around you by knowing the facts and taking appropriate precautions. Floow advice provided by your local health authority.
+        </div>
+
+        <div className="card-section">
+
+          <div className="card">
+            <div className="img">
+              <img src="/amico_1.png" />
+            </div>
+            <div className="card-title">
+              Facebook
+            </div>
+            <div className="desc">
+              Masks can help prevent the spread of the virus from the person wearing the mask to others.
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="img">
+              <img src="/amico_2.png" />
+            </div>
+            <div className="card-title">
+              Hội người già
+            </div>
+            <div className="desc">
+              Clean your hands with soop and water, or alcohol-based hand sanitizer.
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="img">
+              <img src="/amico_3.png" />
+            </div>
+            <div className="card-title">
+              Bệnh viện
+            </div>
+            <div className="desc">
+              Maintain a safe distance especially from anyone who is coughinf or sneezing.
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+      <div className="contact-us">
+        <div className="box">
+          <div className="title">Để lại thông tin để chúng tôi liên hệ với bạn</div>
+          <div className="hr"></div>
+          <div className="desc-title">
+            Protect yourself and others around you by knowing the facts and taking appropriate precautions. Floow advice provided by your local health authority.
+          </div>
+          <div className="img-cover">
+            <div className="img"></div>
           </div>
         </div>
       </div>
